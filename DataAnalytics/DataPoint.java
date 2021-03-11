cd136dba709cd160e37df2eb603c14ffc3762267
Xu Xue Yu <xuxueyu@XudeMacBook-Air.local>
2021-03-11 14:41:06 -0800

    Added the datapoint.java

---
+++ DataAnalytics/DataPoint.java
@@ -0,0 +1,87 @@
+package project;
+
+public class DataPoint 
+{
+	private double f1;
+	private double f2;
+	private String label;
+	private boolean isTest;
+	
+	public DataPoint(double f1, double f2, String label, boolean test)
+	{
+		this.f1 = f1;
+		this.f2 = f2;
+		setLabel(label);
+		this.isTest = test;
+	}
+	
+	public DataPoint(double f1, double f2)
+	{
+		this.f1 = f1;
+		this.f2 = f2;
+		this.label = null;
+		this.isTest = false;
+	}
+	
+	public DataPoint()
+	{
+		this.f1 = 0.0;
+		this.f2 = 0.0;
+		this.label = null;
+		this.isTest = false;
+	}
+	
+	public Double getF1()
+	{
+		return this.f1;
+	}
+	
+	public double getF2()
+	{
+		return this.f2;
+	}
+	
+	public String getLabel()
+	{
+		return this.label;
+	}
+	
+	public boolean getIsTest()
+	{
+		return this.isTest;
+	}
+	
+	public void setF1(Double f)
+	{
+		if(f < 0)
+		{
+			return;
+		}
+		this.f1 = f;
+	}
+	
+	public void setF2(double f)
+	{
+		if(f < 0)
+		{
+			return;
+		}
+		this.f2 = f;
+	}
+	
+	public void setLabel(String label)
+	{
+		if(!(label.equals("Green") || label.equals("Blue")))
+		{
+			this.label = null;
+			return;
+		}
+		this.label = label;
+	}
+	
+	public void setIsTest(boolean t)
+	{
+		this.isTest = t;
+	}
+
+}
